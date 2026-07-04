be9f245fd74c5b732e31e47f3ab1eda6c4077b98
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-07-04 10:45:36 +0530

    Create 58-LengthOfLastWord

---
+++ 58-LengthOfLastWord/58-LengthOfLastWord.py
@@ -0,0 +1,14 @@
+# Last updated: 7/4/2026, 10:45:31 AM
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        i = len(s) - 1
+        count = 0
+
+        while i >= 0 and s[i] == ' ':
+            i -= 1
+
+        while i >= 0 and s[i] != ' ':
+            count += 1
+            i -= 1
+
+        return count
