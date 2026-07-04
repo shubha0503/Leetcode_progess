e341cf4a6633e0df23ddf57d8d7aeb378e5b6238
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-07-04 10:42:54 +0530

    Create 1984-MaximumDistanceBetweenAPairOfValues

---
+++ 1984-MaximumDistanceBetweenAPairOfValues/1984-MaximumDistanceBetweenAPairOfValues.py
@@ -0,0 +1,15 @@
+# Last updated: 7/4/2026, 10:42:49 AM
+class Solution:
+  def maxDistance(self, nums1: list[int], nums2: list[int]) -> int:
+    ans = 0
+    i = 0
+    j = 0
+
+    while i < len(nums1) and j < len(nums2):
+      if nums1[i] > nums2[j]:
+        i += 1
+      else:
+        ans = max(ans, j - i)
+        j += 1
+
+    return ans
