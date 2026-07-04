240dd505c82b25497214655f862057741b1ceb3e
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-07-04 10:42:36 +0530

    Create 2199-TwoFurthestHousesWithDifferentColors

---
+++ 2199-TwoFurthestHousesWithDifferentColors/2199-TwoFurthestHousesWithDifferentColors.py
@@ -0,0 +1,12 @@
+# Last updated: 7/4/2026, 10:42:31 AM
+class Solution:
+    def maxDistance(self, colors: List[int]) -> int:
+        n = len(colors)
+        if colors[0] != colors[-1]:
+            return n - 1
+        i, j = 1, n - 2
+        while colors[i] == colors[0]:
+            i += 1
+        while colors[j] == colors[0]:
+            j -= 1
+        return max(n - i - 1, j)
