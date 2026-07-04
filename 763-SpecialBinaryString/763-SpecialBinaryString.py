60eb8b03719f24be631a5cb7147870e519e5904a
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-07-04 10:44:24 +0530

    Create 763-SpecialBinaryString

---
+++ 763-SpecialBinaryString/763-SpecialBinaryString.py
@@ -0,0 +1,15 @@
+# Last updated: 7/4/2026, 10:44:19 AM
+class Solution:
+  def makeLargestSpecial(self, s: str) -> str:
+    specials = []
+    count = 0
+
+    i = 0
+    for j, c in enumerate(s):
+      count += 1 if c == '1' else -1
+      if count == 0:
+        specials.append(
+            '1' + self.makeLargestSpecial(s[i + 1:j]) + '0')
+        i = j + 1
+
+    return ''.join(sorted(specials)[::-1])
