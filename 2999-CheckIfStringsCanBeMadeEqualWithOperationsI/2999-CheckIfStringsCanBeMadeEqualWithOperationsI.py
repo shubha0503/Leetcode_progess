a58f61ec5cf23a8a856d1d22f6a9048380bdb8b5
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-07-04 10:41:59 +0530

    Create 2999-CheckIfStringsCanBeMadeEqualWithOperationsI

---
+++ 2999-CheckIfStringsCanBeMadeEqualWithOperationsI/2999-CheckIfStringsCanBeMadeEqualWithOperationsI.py
@@ -0,0 +1,13 @@
+# Last updated: 7/4/2026, 10:41:54 AM
+class Solution:
+  def canBeEqual(self, s1: str, s2: str) -> bool:
+    def swappedStrings(s: str) -> list[str]:
+      chars = list(s)
+      return [chars,
+              ''.join([chars[2], chars[1], chars[0], chars[3]]),
+              ''.join([chars[0], chars[3], chars[2], chars[1]]),
+              ''.join([chars[2], chars[3], chars[0], chars[1]])]
+
+    return any(a == b
+               for a in swappedStrings(s1)
+               for b in swappedStrings(s2))
