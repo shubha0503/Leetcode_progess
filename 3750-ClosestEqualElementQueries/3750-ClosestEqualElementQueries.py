b7bb6019e0d89b97bb55eca754a029c279fc5208
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-07-04 10:41:23 +0530

    Create 3750-ClosestEqualElementQueries

---
+++ 3750-ClosestEqualElementQueries/3750-ClosestEqualElementQueries.py
@@ -0,0 +1,22 @@
+# Last updated: 7/4/2026, 10:41:18 AM
+class Solution:
+  def solveQueries(self, nums: list[int], queries: list[int]) -> list[int]:
+    n = len(nums)
+    # minDist[i] := the minimum distance between nums[i], and any other index j
+    # in the circular array, where nums[j] == nums[i]
+    minDist = [n] * n
+    lastSeen = {}
+
+    for i in range(n * 2):
+      index = i % n
+      num = nums[index]
+      if num in lastSeen:
+        prevIndex = lastSeen[num] % n
+        d = i - prevIndex
+        minDist[index] = min(minDist[index], d)
+        minDist[prevIndex] = min(minDist[prevIndex], d)
+      lastSeen[num] = i
+
+    return [-1 if minDist[query] == n
+            else minDist[query]
+            for query in queries]
