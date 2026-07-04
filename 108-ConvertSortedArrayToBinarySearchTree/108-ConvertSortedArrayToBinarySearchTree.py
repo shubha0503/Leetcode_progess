fa9dc5424525a5b1cab2f98900a1924d8f31e5e9
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-07-04 10:45:07 +0530

    Create 108-ConvertSortedArrayToBinarySearchTree

---
+++ 108-ConvertSortedArrayToBinarySearchTree/108-ConvertSortedArrayToBinarySearchTree.py
@@ -0,0 +1,13 @@
+# Last updated: 7/4/2026, 10:45:02 AM
+class Solution:
+    def sortedArrayToBST(self, nums):
+        if not nums:
+            return None
+        
+        mid = len(nums) // 2
+        
+        root = TreeNode(nums[mid])
+        root.left = self.sortedArrayToBST(nums[:mid])
+        root.right = self.sortedArrayToBST(nums[mid+1:])
+        
+        return root
