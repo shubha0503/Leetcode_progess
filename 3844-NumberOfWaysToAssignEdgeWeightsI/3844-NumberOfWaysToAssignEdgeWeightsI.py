d2acbe483aed02994e6a56a34005a68233aaeb86
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-07-04 10:41:16 +0530

    Create 3844-NumberOfWaysToAssignEdgeWeightsI

---
+++ 3844-NumberOfWaysToAssignEdgeWeightsI/3844-NumberOfWaysToAssignEdgeWeightsI.py
@@ -0,0 +1,17 @@
+# Last updated: 7/4/2026, 10:41:11 AM
+class Solution:
+    def assignEdgeWeights(self, edges: List[List[int]]) -> int:
+        def dfs(i: int, fa: int = 0) -> int:
+            res = 0
+            for j in g[i]:
+                if j != fa:
+                    res = max(res, dfs(j, i) + 1)
+            return res
+
+        n = len(edges) + 1
+        g = [[] for _ in range(n + 1)]
+        for u, v in edges:
+            g[u].append(v)
+            g[v].append(u)
+        d = dfs(1)
+        return pow(2, d - 1, 10**9 + 7)
