e96b1e855cd573ae722bd92e39742740388b07f3
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-07-04 10:43:39 +0530

    Create 1516-TheKThLexicographicalStringOfAllHappyStringsOfLengthN

---
+++ 1516-TheKThLexicographicalStringOfAllHappyStringsOfLengthN/1516-TheKThLexicographicalStringOfAllHappyStringsOfLengthN.py
@@ -0,0 +1,19 @@
+# Last updated: 7/4/2026, 10:43:34 AM
+class Solution:
+    def getHappyString(self, n: int, k: int) -> str:
+        def dfs():
+            if len(s) == n:
+                ans.append("".join(s))
+                return
+            if len(ans) >= k:
+                return
+            for c in "abc":
+                if not s or s[-1] != c:
+                    s.append(c)
+                    dfs()
+                    s.pop()
+
+        ans = []
+        s = []
+        dfs()
+        return "" if len(ans) < k else ans[k - 1]
