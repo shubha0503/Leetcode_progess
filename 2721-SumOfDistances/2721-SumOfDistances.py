c6701b3a2e346cb164bf647d24948d82ef74dad6
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-07-04 10:42:09 +0530

    Create 2721-SumOfDistances

---
+++ 2721-SumOfDistances/2721-SumOfDistances.py
@@ -0,0 +1,15 @@
+# Last updated: 7/4/2026, 10:42:04 AM
+class Solution:
+    def distance(self, nums: List[int]) -> List[int]:
+        d = defaultdict(list)
+        for i, x in enumerate(nums):
+            d[x].append(i)
+        ans = [0] * len(nums)
+        for idx in d.values():
+            left, right = 0, sum(idx) - len(idx) * idx[0]
+            for i in range(len(idx)):
+                ans[idx[i]] = left + right
+                if i + 1 < len(idx):
+                    left += (idx[i + 1] - idx[i]) * (i + 1)
+                    right -= (idx[i + 1] - idx[i]) * (len(idx) - i - 1)
+        return ans
