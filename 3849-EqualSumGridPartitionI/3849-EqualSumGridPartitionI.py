6d245930e7f6b37f0e1b07e57dd00233cd339937
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-07-04 10:41:14 +0530

    Create 3849-EqualSumGridPartitionI

---
+++ 3849-EqualSumGridPartitionI/3849-EqualSumGridPartitionI.py
@@ -0,0 +1,17 @@
+# Last updated: 7/4/2026, 10:41:09 AM
+class Solution:
+    def canPartitionGrid(self, grid: List[List[int]]) -> bool:
+        s = sum(sum(row) for row in grid)
+        if s % 2:
+            return False
+        pre = 0
+        for i, row in enumerate(grid):
+            pre += sum(row)
+            if pre * 2 == s and i != len(grid) - 1:
+                return True
+        pre = 0
+        for j, col in enumerate(zip(*grid)):
+            pre += sum(col)
+            if pre * 2 == s and j != len(grid[0]) - 1:
+                return True
+        return False
