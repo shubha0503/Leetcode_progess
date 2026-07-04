24740f2227f31b38221bd04e0f6b2095dd8e0f27
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-07-04 10:44:52 +0530

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.py
@@ -0,0 +1,21 @@
+# Last updated: 7/4/2026, 10:44:46 AM
+class Solution:
+    def isPalindrome(self, s):
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+            # Skip non-alphanumeric characters
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+
+            # Compare characters (case-insensitive)
+            if s[left].lower() != s[right].lower():
+                return False
+
+            left += 1
+            right -= 1
+
+        return True
