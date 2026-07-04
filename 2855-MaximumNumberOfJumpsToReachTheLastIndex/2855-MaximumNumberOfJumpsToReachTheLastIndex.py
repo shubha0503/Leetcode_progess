d198bdf015329868660f7f24a148745bd689b25b
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-07-04 10:42:05 +0530

    Create 2855-MaximumNumberOfJumpsToReachTheLastIndex

---
+++ 2855-MaximumNumberOfJumpsToReachTheLastIndex/2855-MaximumNumberOfJumpsToReachTheLastIndex.py
@@ -0,0 +1,16 @@
+# Last updated: 7/4/2026, 10:42:00 AM
+class Solution:
+    def maximumJumps(self, nums: List[int], target: int) -> int:
+        @cache
+        def dfs(i: int) -> int:
+            if i == n - 1:
+                return 0
+            ans = -inf
+            for j in range(i + 1, n):
+                if abs(nums[i] - nums[j]) <= target:
+                    ans = max(ans, 1 + dfs(j))
+            return ans
+
+        n = len(nums)
+        ans = dfs(0)
+        return -1 if ans < 0 else ans
