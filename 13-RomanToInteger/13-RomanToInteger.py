b51312981499db511aa18250ac6dcb38d08905c7
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-07-04 10:46:04 +0530

    Create 13-RomanToInteger

---
+++ 13-RomanToInteger/13-RomanToInteger.py
@@ -0,0 +1,11 @@
+# Last updated: 7/4/2026, 10:45:59 AM
+class Solution(object):
+    def romanToInt(self, s):
+       roman = {'I': 1,'V': 5,'X': 10,'L': 50,'C':100,'D': 500,'M': 1000}
+       total = 0
+       for i in range(len(s)):
+        if i+1 < len(s) and roman[s[i]] < roman[s[i+1]]:
+            total -= roman[s[i]]
+        else:
+            total +=roman[s[i]]
+       return total
