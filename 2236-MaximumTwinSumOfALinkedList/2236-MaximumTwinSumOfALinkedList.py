579efe59c962e25e5a5b89a6b31954076418596a
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-07-04 10:42:33 +0530

    Create 2236-MaximumTwinSumOfALinkedList

---
+++ 2236-MaximumTwinSumOfALinkedList/2236-MaximumTwinSumOfALinkedList.py
@@ -0,0 +1,14 @@
+# Last updated: 7/4/2026, 10:42:28 AM
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def pairSum(self, head: Optional[ListNode]) -> int:
+        s = []
+        while head:
+            s.append(head.val)
+            head = head.next
+        n = len(s)
+        return max(s[i] + s[-(i + 1)] for i in range(n >> 1))
