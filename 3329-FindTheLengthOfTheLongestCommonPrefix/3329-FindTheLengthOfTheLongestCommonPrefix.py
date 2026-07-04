8bc600c2d9b65dfe89cd2f038eff31303a92e1f2
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-07-04 10:41:47 +0530

    Create 3329-FindTheLengthOfTheLongestCommonPrefix

---
+++ 3329-FindTheLengthOfTheLongestCommonPrefix/3329-FindTheLengthOfTheLongestCommonPrefix.py
@@ -0,0 +1,16 @@
+# Last updated: 7/4/2026, 10:41:42 AM
+class Solution:
+    def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
+        s = set()
+        for x in arr1:
+            while x:
+                s.add(x)
+                x //= 10
+        mx = 0
+        for x in arr2:
+            while x:
+                if x in s:
+                    mx = max(mx, x)
+                    break
+                x //= 10
+        return len(str(mx)) if mx else 0
