8e0d49f4432ab27f5d5b9faa2d723d2901819055
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-07-04 10:40:14 +0530

    Create 4216-WeightedWordMapping

---
+++ 4216-WeightedWordMapping/4216-WeightedWordMapping.py
@@ -0,0 +1,8 @@
+# Last updated: 7/4/2026, 10:40:09 AM
+class Solution:
+    def mapWordWeights(self, words: List[str], weights: List[int]) -> str:
+        ans = []
+        for w in words:
+            s = sum(weights[ord(c) - ord('a')] for c in w)
+            ans.append(ascii_lowercase[25 - s % 26])
+        return ''.join(ans)
