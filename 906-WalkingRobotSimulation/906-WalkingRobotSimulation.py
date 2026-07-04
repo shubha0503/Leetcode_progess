e533cf8964e1bd7f53d3ff7e73f51b74dafed70d
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-07-04 10:44:13 +0530

    Create 906-WalkingRobotSimulation

---
+++ 906-WalkingRobotSimulation/906-WalkingRobotSimulation.py
@@ -0,0 +1,20 @@
+# Last updated: 7/4/2026, 10:44:08 AM
+class Solution:
+    def robotSim(self, commands: List[int], obstacles: List[List[int]]) -> int:
+        dirs = (0, 1, 0, -1, 0)
+        s = {(x, y) for x, y in obstacles}
+        ans = k = 0
+        x = y = 0
+        for c in commands:
+            if c == -2:
+                k = (k + 3) % 4
+            elif c == -1:
+                k = (k + 1) % 4
+            else:
+                for _ in range(c):
+                    nx, ny = x + dirs[k], y + dirs[k + 1]
+                    if (nx, ny) in s:
+                        break
+                    x, y = nx, ny
+                    ans = max(ans, x * x + y * y)
+        return ans
