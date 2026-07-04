26b369bfb90639b17971083bef627a807291a16e
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-07-04 10:41:49 +0530

    Create 3299-FindTheMaximumNumberOfElementsInSubset

---
+++ 3299-FindTheMaximumNumberOfElementsInSubset/3299-FindTheMaximumNumberOfElementsInSubset.py
@@ -0,0 +1,14 @@
+# Last updated: 7/4/2026, 10:41:43 AM
+class Solution:
+    def maximumLength(self, nums: List[int]) -> int:
+        cnt = Counter(nums)
+        ans = cnt[1] - (cnt[1] % 2 ^ 1)
+        del cnt[1]
+        for x in cnt:
+            t = 0
+            while cnt[x] > 1:
+                x = x * x
+                t += 2
+            t += 1 if cnt[x] else -1
+            ans = max(ans, t)
+        return ans
