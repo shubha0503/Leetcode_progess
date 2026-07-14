3b89d130a2ec8ccfcd814a6fca2224ef229c9bbd
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-07-14 10:09:33 +0530

    Create 2793-CountTheNumberOfCompleteComponents

---
+++ 2793-CountTheNumberOfCompleteComponents/2793-CountTheNumberOfCompleteComponents.py
@@ -0,0 +1,25 @@
+# Last updated: 7/14/2026, 10:09:32 AM
+class Solution:
+    def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
+        def dfs(i: int) -> (int, int):
+            vis[i] = True
+            x, y = 1, len(g[i])
+            for j in g[i]:
+                if not vis[j]:
+                    a, b = dfs(j)
+                    x += a
+                    y += b
+            return x, y
+
+        g = defaultdict(list)
+        for a, b in edges:
+            g[a].append(b)
+            g[b].append(a)
+        vis = [False] * n
+        ans = 0
+        for i in range(n):
+            if not vis[i]:
+                a, b = dfs(i)
+                ans += a * (a - 1) == b
+        return ans
+        
