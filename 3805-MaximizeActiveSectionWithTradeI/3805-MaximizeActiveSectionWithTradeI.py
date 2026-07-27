53ead1269890a169a58bba916899171ee271bf79
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-07-27 09:51:09 +0530

    Create 3805-MaximizeActiveSectionWithTradeI

---
+++ 3805-MaximizeActiveSectionWithTradeI/3805-MaximizeActiveSectionWithTradeI.py
@@ -0,0 +1,19 @@
+# Last updated: 7/27/2026, 9:51:01 AM
+class Solution:
+    def maxActiveSectionsAfterTrade(self, s: str) -> int:
+        n = len(s)
+        ans = i = 0
+        pre, mx = -inf, 0
+        while i < n:
+            j = i + 1
+            while j < n and s[j] == s[i]:
+                j += 1
+            cur = j - i
+            if s[i] == "1":
+                ans += cur
+            else:
+                mx = max(mx, pre + cur)
+                pre = cur
+            i = j
+        ans += mx
+        return ans
