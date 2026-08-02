ba0527de1aac954739363d8ddc9aa1a2e6bee563
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-08-02 11:22:50 +0530

    Create 1386-Shift2dGrid

---
+++ 1386-Shift2dGrid/1386-Shift2dGrid.py
@@ -0,0 +1,10 @@
+# Last updated: 8/2/2026, 11:22:49 AM
+class Solution:
+    def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
+        m, n = len(grid), len(grid[0])
+        ans = [[0] * n for _ in range(m)]
+        for i, row in enumerate(grid):
+            for j, v in enumerate(row):
+                x, y = divmod((i * n + j + k) % (m * n), n)
+                ans[x][y] = v
+        return ans
