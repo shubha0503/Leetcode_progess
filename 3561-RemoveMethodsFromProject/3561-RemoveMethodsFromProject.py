2712751f80e73b4300563e1885e674bbab37730d
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-08-08 10:49:02 +0530

    Create 3561-RemoveMethodsFromProject

---
+++ 3561-RemoveMethodsFromProject/3561-RemoveMethodsFromProject.py
@@ -0,0 +1,33 @@
+# Last updated: 8/8/2026, 10:48:57 AM
+class Solution:
+    def remainingMethods(
+        self, n: int, k: int, invocations: List[List[int]]
+    ) -> List[int]:
+        def dfs(i: int):
+            suspicious[i] = True
+            for j in g[i]:
+                if not suspicious[j]:
+                    dfs(j)
+
+        def dfs2(i: int):
+            vis[i] = True
+            for j in f[i]:
+                if not vis[j]:
+                    suspicious[j] = False
+                    dfs2(j)
+
+        f = [[] for _ in range(n)]
+        g = [[] for _ in range(n)]
+        for a, b in invocations:
+            f[a].append(b)
+            f[b].append(a)
+            g[a].append(b)
+        suspicious = [False] * n
+        dfs(k)
+
+        vis = [False] * n
+        ans = []
+        for i in range(n):
+            if not suspicious[i] and not vis[i]:
+                dfs2(i)
+        return [i for i in range(n) if not suspicious[i]]
