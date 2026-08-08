232a9755abd13e833b1a618f1163228eba717c32
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-08-08 10:48:38 +0530

    Create 3820-NumberOfUniqueXorTripletsIi

---
+++ 3820-NumberOfUniqueXorTripletsIi/3820-NumberOfUniqueXorTripletsIi.py
@@ -0,0 +1,14 @@
+# Last updated: 8/8/2026, 10:48:34 AM
+class Solution:
+    def uniqueXorTriplets(self, nums: List[int]) -> int:
+        mx = max(nums) << 1
+        st = [False] * mx
+        for a in nums:
+            for b in nums:
+                st[a ^ b] = True
+        s = [0] * mx
+        for ab in range(mx):
+            if st[ab]:
+                for c in nums:
+                    s[ab ^ c] = 1
+        return sum(s)
