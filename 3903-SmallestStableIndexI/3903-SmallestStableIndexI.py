36b6f5d96b7f526357ab7a1ff702fdcf5779ceaa
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-09-04 23:36:52 +0530

    Create 3903-SmallestStableIndexI

---
+++ 3903-SmallestStableIndexI/3903-SmallestStableIndexI.py
@@ -0,0 +1,13 @@
+# Last updated: 9/4/2026, 11:36:50 PM
+1class Solution:
+2    def firstStableIndex(self, nums: list[int], k: int) -> int:
+3        n = len(nums)
+4        right = [nums[-1]] * n
+5        for i in range(n - 2, -1, -1):
+6            right[i] = min(right[i + 1], nums[i])
+7        left = 0
+8        for i, x in enumerate(nums):
+9            left = max(left, x)
+10            if left - right[i] <= k:
+11                return i
+12        return -1
