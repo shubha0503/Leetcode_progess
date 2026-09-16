b88034552683473d863a96be6def6d45d0b5830b
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-09-16 11:55:56 +0530

    Create 2091-RemovingMinimumAndMaximumFromArray

---
+++ 2091-RemovingMinimumAndMaximumFromArray/2091-RemovingMinimumAndMaximumFromArray.py
@@ -0,0 +1,12 @@
+# Last updated: 9/16/2026, 11:55:51 AM
+class Solution:
+    def minimumDeletions(self, nums: List[int]) -> int:
+        mi = mx = 0
+        for i, num in enumerate(nums):
+            if num < nums[mi]:
+                mi = i
+            if num > nums[mx]:
+                mx = i
+        if mi > mx:
+            mi, mx = mx, mi
+        return min(mx + 1, len(nums) - mi, mi + 1 + len(nums) - mx)
