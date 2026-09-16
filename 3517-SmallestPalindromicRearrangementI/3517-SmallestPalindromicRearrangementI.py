bacafef7a8caa12f988dfda2772fdc46a06232b2
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-09-16 11:52:25 +0530

    Create 3517-SmallestPalindromicRearrangementI

---
+++ 3517-SmallestPalindromicRearrangementI/3517-SmallestPalindromicRearrangementI.py
@@ -0,0 +1,15 @@
+# Last updated: 9/16/2026, 11:52:21 AM
+class Solution:
+    def smallestPalindrome(self, s: str) -> str:
+        cnt = Counter(s)
+        t = []
+        ch = ""
+        for c in ascii_lowercase:
+            v = cnt[c] // 2
+            t.append(c * v)
+            cnt[c] -= v * 2
+            if cnt[c] == 1:
+                ch = c
+        ans = "".join(t)
+        ans = ans + ch + ans[::-1]
+        return ans
