e6b3bdae774971d88a35e34517a983a78ce3ecf7
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-09-16 11:58:32 +0530

    Create 1358-NumberOfSubstringsContainingAllThreeCharacters

---
+++ 1358-NumberOfSubstringsContainingAllThreeCharacters/1358-NumberOfSubstringsContainingAllThreeCharacters.py
@@ -0,0 +1,9 @@
+# Last updated: 9/16/2026, 11:58:27 AM
+class Solution:
+    def numberOfSubstrings(self, s: str) -> int:
+        d = {"a": -1, "b": -1, "c": -1}
+        ans = 0
+        for i, c in enumerate(s):
+            d[c] = i
+            ans += min(d["a"], d["b"], d["c"]) + 1
+        return ans
