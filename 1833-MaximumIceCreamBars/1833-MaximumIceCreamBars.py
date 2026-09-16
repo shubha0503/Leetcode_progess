8343248c63828a4691a5918163976367924d6657
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-09-16 11:57:00 +0530

    Create 1833-MaximumIceCreamBars

---
+++ 1833-MaximumIceCreamBars/1833-MaximumIceCreamBars.py
@@ -0,0 +1,9 @@
+# Last updated: 9/16/2026, 11:56:55 AM
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        costs.sort()
+        for i, c in enumerate(costs):
+            if coins < c:
+                return i
+            coins -= c
+        return len(costs)
