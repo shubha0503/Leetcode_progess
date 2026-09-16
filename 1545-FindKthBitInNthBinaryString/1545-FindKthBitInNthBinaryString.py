3aea8bae21e0f22891e610a9392da776825a485e
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-09-16 11:57:56 +0530

    Create 1545-FindKthBitInNthBinaryString

---
+++ 1545-FindKthBitInNthBinaryString/1545-FindKthBitInNthBinaryString.py
@@ -0,0 +1,11 @@
+# Last updated: 9/16/2026, 11:57:52 AM
+class Solution:
+  def findKthBit(self, n: int, k: int) -> str:
+    if n == 1:
+      return '0'
+    midIndex = pow(2, n - 1)  # 1-indexed
+    if k == midIndex:
+      return '1'
+    if k < midIndex:
+      return self.findKthBit(n - 1, k)
+    return '1' if self.findKthBit(n - 1, midIndex * 2 - k) == '0' else '0'
