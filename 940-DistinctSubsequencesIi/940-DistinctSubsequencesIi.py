ede51a512dac186e4a57716906bb257355e66759
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-09-16 11:59:30 +0530

    Create 940-DistinctSubsequencesIi

---
+++ 940-DistinctSubsequencesIi/940-DistinctSubsequencesIi.py
@@ -0,0 +1,8 @@
+# Last updated: 9/16/2026, 11:59:26 AM
+class Solution:
+    def distinctSubseqII(self, s: str) -> int:
+        mod = 10**9 + 7
+        f = [0] * 26
+        for c in s:
+            f[ord(c) - ord("a")] = (sum(f) + 1) % mod
+        return sum(f) % mod
