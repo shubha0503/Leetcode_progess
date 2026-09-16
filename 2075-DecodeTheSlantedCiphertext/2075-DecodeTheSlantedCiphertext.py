2408be345a27e83a0c89674d2e0f9c15bb47cbfe
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-09-16 11:56:00 +0530

    Create 2075-DecodeTheSlantedCiphertext

---
+++ 2075-DecodeTheSlantedCiphertext/2075-DecodeTheSlantedCiphertext.py
@@ -0,0 +1,11 @@
+# Last updated: 9/16/2026, 11:55:56 AM
+class Solution:
+    def decodeCiphertext(self, encodedText: str, rows: int) -> str:
+        ans = []
+        cols = len(encodedText) // rows
+        for j in range(cols):
+            x, y = 0, j
+            while x < rows and y < cols:
+                ans.append(encodedText[x * cols + y])
+                x, y = x + 1, y + 1
+        return ''.join(ans).rstrip()
