9f201f388325b8d1ce5930efe8d1695c5f004e50
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-09-16 11:51:45 +0530

    Create 3622-CheckDivisibilityByDigitSumAndProduct

---
+++ 3622-CheckDivisibilityByDigitSumAndProduct/3622-CheckDivisibilityByDigitSumAndProduct.py
@@ -0,0 +1,10 @@
+# Last updated: 9/16/2026, 11:51:41 AM
+class Solution:
+    def checkDivisibility(self, n: int) -> bool:
+        s, p = 0, 1
+        x = n
+        while x:
+            x, v = divmod(x, 10)
+            s += v
+            p *= v
+        return n % (s + p) == 0
