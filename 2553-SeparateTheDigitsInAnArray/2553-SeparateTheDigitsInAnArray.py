3874cdc6e53d825bcc9f1c2955fa7119289c7c8a
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-09-16 11:55:10 +0530

    Create 2553-SeparateTheDigitsInAnArray

---
+++ 2553-SeparateTheDigitsInAnArray/2553-SeparateTheDigitsInAnArray.py
@@ -0,0 +1,11 @@
+# Last updated: 9/16/2026, 11:55:06 AM
+class Solution:
+    def separateDigits(self, nums: List[int]) -> List[int]:
+        ans = []
+        for x in nums:
+            t = []
+            while x:
+                t.append(x % 10)
+                x //= 10
+            ans.extend(t[::-1])
+        return ans
