afbe76c6ee8f133a1568b170934b6be92fa29058
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-09-16 11:58:12 +0530

    Create 1464-MaximumProductOfTwoElementsInAnArray

---
+++ 1464-MaximumProductOfTwoElementsInAnArray/1464-MaximumProductOfTwoElementsInAnArray.py
@@ -0,0 +1,8 @@
+# Last updated: 9/16/2026, 11:58:08 AM
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        ans = 0
+        for i, a in enumerate(nums):
+            for b in nums[i + 1 :]:
+                ans = max(ans, (a - 1) * (b - 1))
+        return ans
