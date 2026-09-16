1dabeb41a202d90d91361594bfca8395b4f75ea5
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-09-16 11:58:15 +0530

    Create 1406-StoneGameIii

---
+++ 1406-StoneGameIii/1406-StoneGameIii.py
@@ -0,0 +1,20 @@
+# Last updated: 9/16/2026, 11:58:11 AM
+class Solution:
+    def stoneGameIII(self, stoneValue: List[int]) -> str:
+        @cache
+        def dfs(i: int) -> int:
+            if i >= len(stoneValue):
+                return 0
+            ans = -inf
+            s = 0
+            for j in range(i, i + 3):
+                if j >= len(stoneValue):
+                    break
+                s += stoneValue[j]
+                ans = max(ans, s - dfs(j + 1))
+            return ans
+
+        res = dfs(0)
+        if res == 0:
+            return 'Tie'
+        return 'Alice' if res > 0 else 'Bob'
