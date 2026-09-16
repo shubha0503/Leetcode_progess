a4e1e25ca371501475f9b830639a9154eb845740
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-09-16 11:50:36 +0530

    Create 3702-LongestSubsequenceWithNonZeroBitwiseXor

---
+++ 3702-LongestSubsequenceWithNonZeroBitwiseXor/3702-LongestSubsequenceWithNonZeroBitwiseXor.py
@@ -0,0 +1,13 @@
+# Last updated: 9/16/2026, 11:50:32 AM
+class Solution:
+    def longestSubsequence(self, nums: List[int]) -> int:
+        n = len(nums)
+        xor = cnt0 = 0
+        for x in nums:
+            xor ^= x
+            cnt0 += int(x == 0)
+        if xor:
+            return n
+        if cnt0 == n:
+            return 0
+        return n - 1
