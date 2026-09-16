0d64f4c47df7f803511bbc325dd30b35d9b557fd
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-09-16 11:52:32 +0530

    Create 3483-Unique3DigitEvenNumbers

---
+++ 3483-Unique3DigitEvenNumbers/3483-Unique3DigitEvenNumbers.py
@@ -0,0 +1,15 @@
+# Last updated: 9/16/2026, 11:52:28 AM
+class Solution:
+    def totalNumbers(self, digits: List[int]) -> int:
+        s = set()
+        for i, a in enumerate(digits):
+            if a & 1:
+                continue
+            for j, b in enumerate(digits):
+                if i == j:
+                    continue
+                for k, c in enumerate(digits):
+                    if c == 0 or k in (i, j):
+                        continue
+                    s.add(c * 100 + b * 10 + a)
+        return len(s)
