7d30650fd1b4da1a805840817fafbab7cbfcda9a
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-09-16 11:53:54 +0530

    Create 3069-DistributeElementsIntoTwoArraysI

---
+++ 3069-DistributeElementsIntoTwoArraysI/3069-DistributeElementsIntoTwoArraysI.py
@@ -0,0 +1,11 @@
+# Last updated: 9/16/2026, 11:53:50 AM
+class Solution:
+    def resultArray(self, nums: List[int]) -> List[int]:
+        arr1 = [nums[0]]
+        arr2 = [nums[1]]
+        for x in nums[2:]:
+            if arr1[-1] > arr2[-1]:
+                arr1.append(x)
+            else:
+                arr2.append(x)
+        return arr1 + arr2
