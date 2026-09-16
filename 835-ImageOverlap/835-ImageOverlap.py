56d5bd7e060c5beb999fbe97677faf9da43e59ed
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-09-16 11:59:56 +0530

    Create 835-ImageOverlap

---
+++ 835-ImageOverlap/835-ImageOverlap.py
@@ -0,0 +1,13 @@
+# Last updated: 9/16/2026, 11:59:52 AM
+class Solution:
+    def largestOverlap(self, img1: List[List[int]], img2: List[List[int]]) -> int:
+        n = len(img1)
+        cnt = Counter()
+        for i in range(n):
+            for j in range(n):
+                if img1[i][j]:
+                    for h in range(n):
+                        for k in range(n):
+                            if img2[h][k]:
+                                cnt[(i - h, j - k)] += 1
+        return max(cnt.values()) if cnt else 0
