b9b5e0f0fe4a5324c1366e8d9d6e765abef6f74c
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-09-16 11:57:14 +0530

    Create 1727-LargestSubmatrixWithRearrangements

---
+++ 1727-LargestSubmatrixWithRearrangements/1727-LargestSubmatrixWithRearrangements.py
@@ -0,0 +1,13 @@
+# Last updated: 9/16/2026, 11:57:10 AM
+class Solution:
+    def largestSubmatrix(self, matrix: List[List[int]]) -> int:
+        for i in range(1, len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j]:
+                    matrix[i][j] = matrix[i - 1][j] + 1
+        ans = 0
+        for row in matrix:
+            row.sort(reverse=True)
+            for j, v in enumerate(row, 1):
+                ans = max(ans, j * v)
+        return ans
