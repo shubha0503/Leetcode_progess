1001a9b092b5512503b1e9223336d3a015abfae2
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-09-16 11:53:43 +0530

    Create 3121-CountTheNumberOfSpecialCharactersIi

---
+++ 3121-CountTheNumberOfSpecialCharactersIi/3121-CountTheNumberOfSpecialCharactersIi.py
@@ -0,0 +1,12 @@
+# Last updated: 9/16/2026, 11:53:39 AM
+class Solution:
+    def numberOfSpecialChars(self, word: str) -> int:
+        first, last = {}, {}
+        for i, c in enumerate(word):
+            if c not in first:
+                first[c] = i
+            last[c] = i
+        return sum(
+            a in last and b in first and last[a] < first[b]
+            for a, b in zip(ascii_lowercase, ascii_uppercase)
+        )
