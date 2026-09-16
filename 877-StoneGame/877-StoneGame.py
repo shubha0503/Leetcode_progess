62b68c352e319883dc08b4e4177ad63508d0694b
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-09-16 11:59:33 +0530

    Create 877-StoneGame

---
+++ 877-StoneGame/877-StoneGame.py
@@ -0,0 +1,10 @@
+# Last updated: 9/16/2026, 11:59:29 AM
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        @cache
+        def dfs(i: int, j: int) -> int:
+            if i > j:
+                return 0
+            return max(piles[i] - dfs(i + 1, j), piles[j] - dfs(i, j - 1))
+
+        return dfs(0, len(piles) - 1) > 0
