bf744872d178e84cfce194ff2e8766cc8a7e4930
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2026-09-16 11:54:44 +0530

    Create 2784-CheckIfArrayIsGood

---
+++ 2784-CheckIfArrayIsGood/2784-CheckIfArrayIsGood.py
@@ -0,0 +1,6 @@
+# Last updated: 9/16/2026, 11:54:39 AM
+class Solution:
+    def isGood(self, nums: List[int]) -> bool:
+        cnt = Counter(nums)
+        n = len(nums) - 1
+        return cnt[n] == 2 and all(cnt[i] for i in range(1, n))
